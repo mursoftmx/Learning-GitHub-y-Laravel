69342ce6f15fc0b7cac0532c548e7045697dde69
Juan Manuel Murillo <mursoft@outlook.com>
2024-01-04 14:08:33 -0700

    Agregar imagen a los usuarios

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'images' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/images'),
+            'root' => storage_path('app/public/img'),
             'url' => env('APP_URL').'/images',
             'visibility' => 'public',
             'throw' => false,
@@ -79,7 +79,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('images') => storage_path('app/public/images'),
+        public_path('images') => storage_path('app/public/img'),
     ],
 
 ];
