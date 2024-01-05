a982b056dcdbc25b2bcc5da669e685ef8d605bc4
Juan Manuel Murillo <mursoft@outlook.com>
2024-01-05 12:35:21 -0700

    Modificacion de la etiqueta Articulos en el Carrito

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                             </li>
                         @endif
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('carts.index') }}">Articulos en el carrito ({{ $cartService->countProducts() }})</a>
+                            <a class="nav-link" href="{{ route('carts.index') }}">Cart Products ({{ $cartService->countProducts() }})</a>
                         </li>
                     </ul>
 
